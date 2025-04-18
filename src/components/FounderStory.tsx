9ee8928d8e34a474ddeebc05a197238aa72fa266
youcefdevs <youcefdevs@gmail.com>
2025-04-18 07:09:09 +0100

    Update FounderStory.tsx
    changed the photo url

--- src/components/FounderStory.tsx
+++ src/components/FounderStory.tsx
@@ -20,7 +20,7 @@ const FounderStory = () => {
             
             <a href="https://x.com/youcef_boun" target="_blank" rel="noopener noreferrer" className="flex items-center mt-8 hover:opacity-90 transition-opacity">
               <img 
-                src="https://x.com/youcef_boun/photo" 
+                src="https://pbs.twimg.com/profile_images/1756381248296042496/C0KV5Q_i_400x400.jpg" 
                 alt="The Builder" 
                 className="h-12 w-12 rounded-full mr-4 object-cover"
               />
